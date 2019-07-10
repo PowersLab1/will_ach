57e27f33a67f32d55e72d01057673b659988e446
Travis Salad <travissalad@level11.com>
2019-07-10 09:16:51 -0700

    replace browser router with hash router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
