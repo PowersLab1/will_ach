401676e231d9078a02aecef0fd0133962ee6353a
jakeburden <jburden@gitlab.com>
2020-09-25 23:03:41 -0400

    Squashed commit of the following:
    commit b3b06565a57cad0dee1a62f7b2bf80a1b7d5730b
    Author: jakeburden <jburden@gitlab.com>
    Date:   Fri Sep 25 23:01:36 2020 -0400
    
        Remove console logs
    
    commit e188195d6e3ed1c003a30b893b082110275a882b
    Author: jakeburden <jburden@gitlab.com>
    Date:   Fri Sep 25 01:41:57 2020 -0400
    
        Add timebox to account for accidental keyup
    
    commit 18542a6a2425b3b865a8579ac615899023a02b9a
    Author: jakeburden <jburden@gitlab.com>
    Date:   Fri Sep 25 00:11:14 2020 -0400
    
        Add formatting
    
    commit 22620d8a62a7c99024c748a8a1e536e73d074d6b
    Author: jakeburden <jburden@gitlab.com>
    Date:   Wed Sep 23 12:30:20 2020 -0400
    
        Move user input listener for confidence bars
    
    Ensure timeboxed key press is prev key before recording
    
    Stop incrementing rating on all keyup events

--- src/containers/Trial.js
+++ src/containers/Trial.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import './Trial.css';
+import "./Trial.css";
 import {
   patch,
   auditoryStim,
   playAuditoryStimulus,
   playWhiteNoise,
   playPinkNoise,
-  playBrownianNoise
+  playBrownianNoise,
 } from "../lib/Stim.js";
-import VisualStimulus from './VisualStimulus';
-import {Redirect} from "react-router-dom";
+import VisualStimulus from "./VisualStimulus";
+import { Redirect } from "react-router-dom";
 
-import {getStore, getEncryptedMetadata, getDataSent} from '../store';
+import { getStore, getEncryptedMetadata, getDataSent } from "../store";
 
-var _ = require('lodash');
-const config = require('../config');
+var _ = require("lodash");
+const config = require("../config");
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const Q_KEY_CODE = 81;
@@ -29,9 +29,8 @@ const KEY_CODE_TO_RATING = {
   53: 5,
 };
 // We want key codes in number form, hence the parseInt
-const RATING_KEY_CODES = _.map(
-  _.keys(KEY_CODE_TO_RATING),
-  (k) => parseInt(k, 10)
+const RATING_KEY_CODES = _.map(_.keys(KEY_CODE_TO_RATING), (k) =>
+  parseInt(k, 10)
 );
 const VISUAL_STIMULUS_MS = 1000;
 const STIMULUS_MS = 1000;
@@ -87,12 +86,15 @@ class Trial extends Component {
     this.componentStartTime = 0;
     this.startTime = 0;
     this.ratingStartTime = 0;
+    this.timebox = null;
+    this.numIterations = 0;
 
     // keydown status
     this.isKeyDown = {
       Q_KEY_CODE: false,
       E_KEY_CODE: false,
-    }
+    };
+    this.prevKey = null;
   }
 
   addTimestamp(eventName) {
@@ -100,7 +102,10 @@ class Trial extends Component {
       this.componentStartTime = new Date().getTime();
       this.timestamps.push([eventName, 0]);
     } else {
-      this.timestamps.push([eventName, new Date().getTime() - this.componentStartTime]);
+      this.timestamps.push([
+        eventName,
+        new Date().getTime() - this.componentStartTime,
+      ]);
     }
   }
 
@@ -136,12 +141,12 @@ class Trial extends Component {
     // Increment index and check if we hit maximum number of attempts,
     // in which case we stop early
     if (that.numAttempts++ == that.numAttemptsLimit) {
-      that.setState({complete: true});
+      that.setState({ complete: true });
       return;
     }
 
     // Start time window for receiving a response
-    that.setState({responseWindow: true});
+    that.setState({ responseWindow: true });
     that.startTime = new Date().getTime();
 
     // Play stimuli
@@ -149,13 +154,16 @@ class Trial extends Component {
     const amp = that.props.decibels[that.state.index];
     playAuditoryStimulus(auditoryStim, that.audioContext, STIMULUS_MS, amp);
 
-    this.stimulusTimer = setTimeout(this.playStimulus, that.delay + that.jitter());
+    this.stimulusTimer = setTimeout(
+      this.playStimulus,
+      that.delay + that.jitter()
+    );
 
     this.addTimestamp("stim");
-  }
+  };
 
   startTrial() {
-    this.setState({trialStarted: true});
+    this.setState({ trialStarted: true });
     this.stimulusTimer = setTimeout(this.playStimulus, this.initialDelay);
     playWhiteNoise(this.audioContext);
     //playBrownianNoise(this.audioContext);
@@ -165,13 +173,13 @@ class Trial extends Component {
   }
 
   jitter() {
-    return Math.random() / 2 * 1000; // in ms
+    return (Math.random() / 2) * 1000; // in ms
   }
 
   shutdown() {
     this.addTimestamp("end");
     this.saveDataToStore();
-    this.setState({complete: true});
+    this.setState({ complete: true });
   }
 
   saveDataToStore() {
@@ -197,14 +205,14 @@ class Trial extends Component {
 
     // If we don't have an id on file, then abort
     if (_.isUndefined(getEncryptedMetadata())) {
-      this.setState({invalid: true});
+      this.setState({ invalid: true });
     }
 
     if (this.state.complete === false) {
       // Oddly enough, we don't see the initial render unless
       // this is scheduled this way.
       setTimeout(() => {
-        this.setState({readyToStart: true});
+        this.setState({ readyToStart: true });
       }, 0);
     }
   }
@@ -219,15 +227,18 @@ class Trial extends Component {
   render() {
     // Something went wrong, so we redirect to error page.
     if (this.state.invalid) {
-      return <Redirect to="/Error" />
+      return <Redirect to="/Error" />;
     } else if (this.state.dataSent) {
       // If we already sent out data, we're done.
-      return <Redirect to="/ThankYou" />
+      return <Redirect to="/ThankYou" />;
     } else if (this.state.complete) {
       // If trial is complete, then we use the renderer passed in as a prop.
       // This renderer should take care of the redirect logic.
 
-      return this.props.trialCompleteRenderer(this.props.decibels, this.response);
+      return this.props.trialCompleteRenderer(
+        this.props.decibels,
+        this.response
+      );
     }
 
     return (
@@ -241,7 +252,6 @@ class Trial extends Component {
             />
           </div>
         ) : (
-
           <p className="Trial-text">
             {this.state.readyToStart ? (
               <span>Press any key to begin</span>
@@ -260,9 +270,72 @@ class Trial extends Component {
    *                              *
    ********************************/
 
+  recordResponse = (event) => {
+    if (
+      this.state.responseWindow &&
+      _.includes([Q_KEY_CODE, E_KEY_CODE], event.keyCode)
+    ) {
+      var that = this;
+
+      // record timestamp
+      this.addTimestamp("resp");
+
+      var ms = new Date().getTime();
+
+      // Record 1 as response if Q, record 0 if E
+      const response = event.keyCode === Q_KEY_CODE ? 1 : 0;
+      this.response.push(response);
+      this.responseTime.push(ms - this.startTime);
+      this.setState({ responseWindow: false });
+      this.timebox = null;
+
+      // Remember to call handler, which is used by the Quest trial
+      this.props.responseHandler(response);
+
+      // If we're also recording ratings, then open the window
+      // for receiving ratings
+      if (this.props.shouldRecordRatings) {
+        clearTimeout(this.stimulusTimer);
+        this.setState({
+          ratingWindow: true,
+          currentRating: that.numIterations || 1,
+          stopShowingRating: false,
+          stopIncrementingRating: false,
+        });
+
+        that.numIterations = that.numIterations || 0;
+        function scheduleRating() {
+          that.numIterations++;
+          that.ratingTimer = setTimeout(() => {
+            if (that.numIterations == 5 || that.state.stopShowingRating) {
+              that.numIterations = 0;
+              that.prevKey = null;
+
+              that.finishRatingWindow();
+              return;
+            }
+            if (!that.state.stopIncrementingRating) {
+              that.setState({ currentRating: that.state.currentRating + 1 });
+            }
+            scheduleRating();
+          }, 250);
+        }
+
+        this.ratingStartTime = new Date().getTime();
+        scheduleRating();
+      } else {
+        // Otherwise, move on to the next index
+        this.prevKey = null;
+        this.setState({ index: this.state.index + 1 });
+      }
+    } else {
+      this.prevKey = null;
+    }
+  };
+
   keyDownFunction = (event) => {
     if (this.state.readyToStart) {
-      this.setState({readyToStart: false});
+      this.setState({ readyToStart: false });
       this.audioContext.resume();
       this.startTrial();
       return;
@@ -278,71 +351,41 @@ class Trial extends Component {
       }
     }
 
-    if (this.state.responseWindow && _.includes([Q_KEY_CODE, E_KEY_CODE], event.keyCode)) {
-      // record timestamp
-      this.addTimestamp("resp");
-
-      var ms = new Date().getTime();
-
-      // Record 1 as response if Q, record 0 if E
-      const response = event.keyCode === Q_KEY_CODE ? 1 : 0;
-      this.response.push(response);
-      this.responseTime.push(ms - this.startTime);
-      this.setState({responseWindow: false});
-
-      // Remember to call handler, which is used by the Quest trial
-      this.props.responseHandler(response);
-
-      // If we're also recording ratings, then open the window
-      // for receiving ratings
-      if (this.props.shouldRecordRatings) {
-        clearTimeout(this.stimulusTimer);
-        this.setState({
-          ratingWindow: true,
-          currentRating: 1,
-          stopShowingRating: false,
-          stopIncrementingRating: false,
-        });
-
-        var that = this;
-        var numIterations = 0;
-        function scheduleRating() {
-          numIterations++;
-          that.ratingTimer = setTimeout(() => {
-            if (numIterations == 5 || that.state.stopShowingRating) {
-              that.finishRatingWindow();
-              return;
-            }
-            if (!that.state.stopIncrementingRating) {
-              that.setState({currentRating: that.state.currentRating + 1});
-            }
-            scheduleRating();
-          }, 250);
-        }
-
-        this.ratingStartTime = new Date().getTime();
-        scheduleRating();
-      } else {
-        // Otherwise, move on to the next index
-        this.setState({index: this.state.index + 1});
-      }
+    if (this.prevKey === null) {
+      this.prevKey = event.keyCode;
     }
-  }
+
+    if (this.prevKey !== event.keyCode) return;
+
+    clearTimeout(this.timebox);
+    this.timebox = setTimeout(() => {
+      this.setState({ stopIncrementingRating: false });
+      this.recordResponse(event);
+    }, 50);
+  };
 
   keyUpFunction = (event) => {
     if (_.includes([Q_KEY_CODE, E_KEY_CODE], event.keyCode)) {
-        this.isKeyDown[event.keyCode] = false;
+      this.isKeyDown[event.keyCode] = false;
     }
 
+    this.setState({ stopIncrementingRating: true });
     if (this.state.ratingWindow) {
-      // Get response key code
-      const responseKeyCode = _.last(this.response) == 1 ? Q_KEY_CODE : E_KEY_CODE;
-      if (responseKeyCode == event.keyCode) {
-        this.addTimestamp("rating");
+      if (this.timebox) {
+        setTimeout(() => {
+          this.timebox = null;
+          this.prevKey = null;
+        }, 250);
+      } else {
+        // Get response key code
+        const responseKeyCode =
+          _.last(this.response) == 1 ? Q_KEY_CODE : E_KEY_CODE;
+        if (responseKeyCode == event.keyCode) {
+          this.addTimestamp("rating");
 
-        var ms = new Date().getTime();
-        this.ratingsRaw.push(ms - this.ratingStartTime);
-        this.setState({stopIncrementingRating: true});
+          var ms = new Date().getTime();
+          this.ratingsRaw.push(ms - this.ratingStartTime);
+        }
       }
     }
   };
@@ -355,7 +398,7 @@ class Trial extends Component {
 
       var ms = new Date().getTime();
       this.ratingsRaw.push(ms - this.ratingStartTime);
-      this.setState({stopIncrementingRating: true});
+      this.setState({ stopIncrementingRating: true });
     }
 
     this.setState({
@@ -363,35 +406,35 @@ class Trial extends Component {
       ratingWindow: false,
     });
     this.stimulusTimer = setTimeout(this.playStimulus, 1000 + this.jitter());
-  }
+  };
 
   // Debugging
   log_debug() {
-    console.log('================================');
-    console.log('all decibels: ' + this.props.decibels);
-    console.log('all responses: ' + this.response);
-    console.log('all responseTime: ' + this.responseTime);
-    console.log('all timestamps: ' + JSON.stringify(this.timestamps));
-    console.log('all ratingsRaw: ' + this.ratingsRaw);
-    console.log('all ratings: ' + this.ratings);
+    console.log("================================");
+    console.log("all decibels: " + this.props.decibels);
+    console.log("all responses: " + this.response);
+    console.log("all responseTime: " + this.responseTime);
+    console.log("all timestamps: " + JSON.stringify(this.timestamps));
+    console.log("all ratingsRaw: " + this.ratingsRaw);
+    console.log("all ratings: " + this.ratings);
 
-    console.log('index: ' + this.state.index);
-    console.log('numAttempts: ' + this.numAttempts);
-    console.log('store: ' + JSON.stringify(getStore()));
-    console.log('localStorage: ' + JSON.stringify(localStorage));
-    console.log('================================\n');
+    console.log("index: " + this.state.index);
+    console.log("numAttempts: " + this.numAttempts);
+    console.log("store: " + JSON.stringify(getStore()));
+    console.log("localStorage: " + JSON.stringify(localStorage));
+    console.log("================================\n");
   }
 } // end class
 
-
 Trial.defaultProps = {
-  decibels: config.debug ? _.shuffle([65, 65, 65, 65]) :
-    _.concat([65], _.shuffle([0, 0, 0, 0, 0, 65, 65, 65, 65])),
+  decibels: config.debug
+    ? _.shuffle([65, 65, 65, 65])
+    : _.concat([65], _.shuffle([0, 0, 0, 0, 0, 65, 65, 65, 65])),
   shouldRecordRatings: false,
   trialCompleteRenderer: _.noop,
   responseHandler: _.noop,
   dataHandler: _.noop,
-}
+};
 
 Trial.propTypes = {
   decibels: PropTypes.array.isRequired,
@@ -399,6 +442,6 @@ Trial.propTypes = {
   trialCompleteRenderer: PropTypes.func,
   responseHandler: PropTypes.func,
   dataHandler: PropTypes.func.isRequired,
-}
+};
 
 export default Trial;
