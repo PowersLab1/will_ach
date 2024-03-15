b2bdddb6f7f94a5a5d8616cb77eb181919b27381
Max Greenwald <powerslab3@gmail.com>
2024-03-15 16:43:56 -0400

    Updates

---
+++ precache-manifest.142dad18963613fb7ce9119caea541c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3aeda77e5f38bbb6eb1682b2fbd5a3",
+    "url": "/ACH_APP/index.html"
+  },
+  {
+    "revision": "431bb6eb7130d0d7bcfc",
+    "url": "/ACH_APP/static/css/main.f4ac948c.chunk.css"
+  },
+  {
+    "revision": "54ffdef82ea47e5f8b4e",
+    "url": "/ACH_APP/static/js/2.d1d70ba9.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/ACH_APP/static/js/2.d1d70ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431bb6eb7130d0d7bcfc",
+    "url": "/ACH_APP/static/js/main.796478f0.chunk.js"
+  },
+  {
+    "revision": "a383fb2989c462b3802b",
+    "url": "/ACH_APP/static/js/runtime-main.006952eb.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ACH_APP/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ACH_APP/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ACH_APP/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ACH_APP/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ACH_APP/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ACH_APP/static/media/rating_keydown_5.34958048.png"
+  }
+]);
